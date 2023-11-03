783d694f5099641d7bba7373557e5c10bfb270fb
sonnguyen106mh <sonnguyen106mh@gmail.com>
2023-11-04 00:21:51 +0700

    save and retrieving data firestore

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -8,20 +8,59 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
-    var messages: [Message] = [ Message(sender: "son@gmail.com", body: "Hello"),Message(sender: "uyen@gmail.com", body: "Hi!"),Message(sender: "son@gmail.com", body: "Nice to meet you ")]
+    let db = Firestore.firestore()
+    var messages: [Message] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
         title = K.appname
+        loadMessage()
         tableView.dataSource = self
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
-    
+    func loadMessage() {
+        
+        db.collection(K.FStore.collectionName).getDocuments() { (querySnapshot, err) in
+          if let err = err {
+            print("Error getting documents: \(err)")
+          } else {
+            for document in querySnapshot!.documents {
+                if  let messageSender = document.data()[K.FStore.senderField] as? String, let messageBody = document.data()[K.FStore.bodyField] as? String {
+                    let newMessage = Message(sender: messageSender, body: messageBody)
+                    self.messages.append(newMessage)
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+                
+            }
+          }
+        }
+
+    }
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageSender = Auth.auth().currentUser?.email, let messageBody = messageTextfield.text  {
+          // User is signed in.
+            var ref: DocumentReference? = nil
+            ref = db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody
+            ]) { err in
+              if let err = err {
+                print("Error adding document: \(err)")
+              } else {
+                print("Document added with ID: \(ref!.documentID)")
+              }
+            }
+        } else {
+          // No user is signed in.
+          // ...
+        }
     }
     
 
